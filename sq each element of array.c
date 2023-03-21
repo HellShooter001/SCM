68ba1be60e781efa6702e061ec2314282a955479
HellShooter001 <127821344+HellShooter001@users.noreply.github.com>
2023-03-21 11:25:57 +0530

    Commit Made

--- sq each element of array.c
+++ sq each element of array.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
 int main(){
  // n->size of array
- int n,i; scanf("%d",&n); 
+ int num,i; scanf("%d",&num); 
  
 // array declaration
- int arr[n];
+ int arr[num];
 // value assign in each index
- for(i=0;i<n;i++){
+ for(i=0;i<num;i++){
  scanf("%d",&arr[i]);
  }
  
  // updating(processing) value after squere each elemnts
- for(i=0;i<n;i++){
+ for(i=0;i<num;i++){
  arr[i]=arr[i]*arr[i];
  }
 // printing each value of array 
-for(i=0;i<n;i++){
+for(i=0;i<num;i++){
 printf("%d ", arr[i]);
 }
 }
